bb911fd33ef48c4165be96090cf9079c3a053df8
Alvin Khair <khairalvin@gmail.com>
2021-11-18 04:41:09 +0700

    init project

---
+++ consumer/consumer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Will Send email body or sms body to the broker
+
+	// Create Connection to the rabbitMQ
+
+	// Declare the Exchange
+
+	// Declare the Queue
+
+	// Bind the Queue with the Exchange
+
+	// Consume from Queue
+
+	// Send the information
+	fmt.Println("send")
+}
---
+++ producer/producer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Will Send email body or sms body to the broker
+
+	// Create Connection to the rabbitMQ
+
+	// Declare the Exchange
+
+	// Create the Body
+
+	// Publish to the Exchange
+	fmt.Println("send")
+}
